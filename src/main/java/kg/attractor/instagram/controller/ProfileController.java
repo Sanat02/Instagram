a1de7730f1a1e7852d2fdd67913f76a1d0339ba9
Sanat Bakytbekova <sanatbakytbekova@gmail.com>
2023-08-29 17:06:14 +0600

    Add functionality to see each post

--- src/main/java/kg/attractor/instagram/controller/ProfileController.java
+++ src/main/java/kg/attractor/instagram/controller/ProfileController.java
@@ -2,7 +2,11 @@ package kg.attractor.instagram.controller;
 
 
 
+import kg.attractor.instagram.dto.ProfileImageDto;
 import kg.attractor.instagram.dto.UserDto;
+import kg.attractor.instagram.service.LikeService;
+import kg.attractor.instagram.service.ProfileImageService;
+import kg.attractor.instagram.service.ProfilePhotoService;
 import kg.attractor.instagram.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -18,6 +22,9 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class ProfileController {
     private final UserService userService;
+    private final ProfileImageService profileImageService;
+    private final LikeService likeService;
+
 
     @GetMapping("/profile")
     public String getProfile(Model model) {
@@ -25,6 +32,17 @@ public class ProfileController {
         UserDto userDto = userService.mapToUserDto(userService.getUserByEmail(auth.getName()).orElse(null));
         model.addAttribute("account",userDto);
         return "profile";
+    }
+
+    @GetMapping("/profile/{imageId}")
+    public String getPost(@PathVariable int imageId, Model model) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        UserDto userDto = userService.mapToUserDto(userService.getUserByEmail(auth.getName()).orElse(null));
+        model.addAttribute("account",userDto);
+        ProfileImageDto profileImageDto=profileImageService.getImageById(imageId);
+        profileImageDto.setLikes(likeService.getLikesById(imageId));
+        model.addAttribute("image",profileImageDto);
+        return "post";
     }
 
 }
